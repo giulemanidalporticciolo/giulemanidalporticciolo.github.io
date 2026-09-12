4881f2a09b68a6186f7d9429b0e4128f018ace0a
giulemanidalporticciolo <rossmsa1@gmail.com>
2026-09-12 07:16:00 +0200

    .

--- sito/progetto/mappa-orientamento2.js
+++ sito/progetto/mappa-orientamento2.js
@@ -224,7 +224,7 @@ function showPlace(marker, shouldScrollPage = true) {
 
             window.scrollTo({
 
-                top: mapTop - 40,
+                top: mapTop - 100,
 
                 behavior: "smooth"
 
